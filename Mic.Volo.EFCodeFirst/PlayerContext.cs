2affcf2bfa6b77967b77b1410eb66dee4f25f1e9
User <artyom.hovakimyan01@gmail.com>
2019-04-28 17:22:29 +0400

    add connectionString name  [name]Context

---
+++ Mic.Volo.EFCodeFirst/PlayerContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mic.Volo.EFCodeFirst
+{
+    class PlayerContext:DbContext
+    {
+        public PlayerContext()
+        {
+
+        }
+        public DbSet<Player> Players { get;set }
+    }
+}
