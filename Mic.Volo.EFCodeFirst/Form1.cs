475cd47730115c938c0dc2a78511c9bfc4eafc27
User <artyom.hovakimyan01@gmail.com>
2019-04-28 17:51:25 +0400

    add player

--- Mic.Volo.EFCodeFirst/Form1.cs
+++ Mic.Volo.EFCodeFirst/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,40 @@ namespace Mic.Volo.EFCodeFirst
 {
     public partial class Form1 : Form
     {
+        PlayerContext db;
         public Form1()
         {
             InitializeComponent();
+            db = new PlayerContext();
+            db.Players.Load();
+            dataGridView1.DataSource = db.Players.Local.ToBindingList();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PlayerForm plForm = new PlayerForm();
+            DialogResult result = plForm.ShowDialog(this);
+            if (result == DialogResult.Cancel)
+                return;
+            Player player = new Player();
+            player.Age = (int)plForm.numericUpDown1.Value;
+            player.Name = plForm.textBox1.Text;
+            player.Position = plForm.comboBox1.SelectedItem.ToString();
+
+            db.Players.Add(player);
+            db.SaveChanges();
+
+            MessageBox.Show("New player is added!");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
